cb4170fac6d8667820d454a00d64b21f95927d17
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-26 13:29:17 +0530

    refactor global scss

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = (): React.ReactElement => {
   return (
-    <div className="w-full h-24 box-border flex gap-8 justify-end items-center px-16">
+    <div className="w-full h-28 box-border flex gap-8 justify-end items-center px-16">
       Footer
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
+import ThemeSwitcher from "./../utils/ThemeSwitcher";
 import React from "react";
 
 const NavBar: React.FC = (): React.ReactElement => {
   return (
-    <div className="w-full h-24 box-border flex gap-8 justify-end items-center px-16">
-      NavBar
+    <div className="w-full h-28 box-border flex gap-8 justify-between items-center px-16">
+      <div></div>
+      <ThemeSwitcher />
     </div>
   );
 };
