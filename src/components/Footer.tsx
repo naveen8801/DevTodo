f676119bcb8ccb418f4e25c9bd83152817f6c1dc
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-26 19:53:40 +0530

    Added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer: React.FC = (): React.ReactElement => {
   return (
     <div className="w-full h-28 box-border flex gap-8 justify-end items-center px-16">
-      Footer
+      <Link
+        href="https://github.com/naveen8801/DevTodo"
+        target="_blank"
+        className="flex gap-2 items-center justify-center text-lg font-semibold hover:font-bold hover:cursor-pointer hover:text-primaryColor"
+      >
+        Source Code
+        <FaGithub size={28} />
+      </Link>
     </div>
   );
 };
