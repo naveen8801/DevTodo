df5dfb704524764a8d19c05bb72d3a1d7c801d00
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-27 13:59:22 +0530

    Added Connect your github account btn

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Dashboard = async () => {
   const session = await getServerSession();
@@ -8,7 +10,18 @@ const Dashboard = async () => {
     redirect("/login");
   }
 
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <div className="h-full flex flex-col items-center justify-center">
+        <Link href={process.env.GITHUB_PUBLIC_URL!}>
+          <button className="px-4 py-2 text-black rounded-lg flex items-center justify-center gap-2 bg-buttonBgColor hover:cursor-pointer hover:font-medium">
+            <FaGithub size={20} />
+            Connect your Github Account
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,13 +6,6 @@ import { handleGithubSignIn } from "@/actions";
 export default function Login() {
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      {/* <div
-        className="px-4 py-2 text-black rounded-lg flex items-center justify-center gap-2 bg-buttonBgColor hover:cursor-pointer hover:font-medium"
-        onClick={handleSignIn}
-      >
-        <FaGithub size={20} />
-        Sign In With Github{" "}
-      </div> */}
       <form action={handleGithubSignIn}>
         <button className="px-4 py-2 text-black rounded-lg flex items-center justify-center gap-2 bg-buttonBgColor hover:cursor-pointer hover:font-medium">
           <FaGithub size={20} />
