83a1e5c8b80b659eb3e813a056dd67d1e7bf46ae
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-27 15:16:48 +0530

    Impleemnted a route to get current user

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,22 @@
+import User from "@/Models/User";
+import connectDB from "@/utils/ConnectDB";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const session = await getServerSession();
+  if (!session || !session.user) {
+    return NextResponse.json({ msg: "Unauthorized" });
+  }
+  const email = session.user.email;
+  connectDB();
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ msg: "No user with this email" });
+    }
+    return NextResponse.json({ data: user });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
