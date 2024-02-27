59a1ff90a7e1dafd00a01fb263d65b9f3773fbd3
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-27 13:25:13 +0530

    Added comments

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,18 @@
 import { signIn, signOut } from "next-auth/react";
 
+/**
+ * Function to handle GitHub sign-in.
+ *
+ * @return {Promise<void>} A promise that resolves when the sign-in is complete.
+ */
 export const handleGithubSignIn = async () => {
   await signIn("github");
 };
+
+/**
+ * Function to handle signing out from Github.
+ *
+ */
 
 export const handleGithubSignOut = async () => {
   await signOut();
