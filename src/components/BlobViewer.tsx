ec88401dd0aae6a0832ce03a5ef09e12e2e36013
naveen8801 <naveen.sharma@stackguardian.io>
2024-03-14 15:28:59 +0530

    Added getShortFocusedContent and getHighlightedText method

--- src/components/BlobViewer.tsx
+++ src/components/BlobViewer.tsx
@@ -15,10 +15,63 @@ const BlobViewer: React.FC<IProp> = async (
 
   const { data, error } = await getGithubBlob(url);
 
+  const getContent = (source: string) => {
+    const a = getShortFocusedContent(source, "TODO");
+    const b = getShortFocusedContent(source, "Todo");
+    const c = getShortFocusedContent(source, "todo");
+    if (a?.length > 0) {
+      return getHighlightedText(a, "TODO");
+    } else if (b?.length > 0) {
+      return getHighlightedText(b, "Todo");
+    } else if (c?.length > 0) {
+      return getHighlightedText(c, "todo");
+    }
+  };
+
+  const getShortFocusedContent = (string: string, target: string) => {
+    const firstOcc = string?.indexOf(target);
+    const lastOcc = string?.lastIndexOf(target);
+    if (firstOcc != -1 && lastOcc != -1) {
+      if (firstOcc === lastOcc) {
+        const mainString = string.slice(
+          firstOcc - 30 > 0 ? firstOcc - 30 : 0,
+          firstOcc + 30 < string?.length - 1
+            ? firstOcc + 30
+            : string?.length - 1
+        );
+        return mainString?.trim();
+      } else {
+        const mainString = string.slice(firstOcc, lastOcc + target?.length);
+        return mainString?.trim();
+      }
+    }
+    return "";
+  };
+
+  const getHighlightedText = (text: string, highlight: string) => {
+    const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+    return (
+      <span>
+        {parts.map((part, i) => (
+          <span
+            key={i}
+            style={
+              part.toLowerCase() === highlight.toLowerCase()
+                ? { fontWeight: "bold", color: "#FF4C29" }
+                : {}
+            }
+          >
+            {part}
+          </span>
+        ))}
+      </span>
+    );
+  };
+
   return (
-    <div className="h-32 overflow-auto p-4">
+    <div className="h-fit overflow-auto p-4">
       <pre className="text-xs dark:text-slate-300">
-        {data ? atob(data) : ""}
+        {data ? getContent(atob(data)) : ""}
       </pre>
     </div>
   );
