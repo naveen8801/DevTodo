37a1bbac079b485e629a1d9f18f46f4377591f2d
naveen8801 <naveen.sharma@stackguardian.io>
2024-03-17 19:30:19 +0530

    Added event type checks inside webhook api handler

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,13 +1,41 @@
 import { handleVerifyGithubSignature } from "@/utils/GithubAPIUtils";
-import { NextApiRequest, NextApiResponse } from "next";
 
 export async function POST(req: Request) {
   try {
-    const body = await req.json();
-    if (handleVerifyGithubSignature(req, body)) {
-      return Response.json({ msg: "Unauthorized" }, { status: 401 });
+    const eventData = await req.json();
+    const eventType = req.headers.get("X-GitHub-Event");
+    console.log({ eventType });
+    if (!eventType) {
+      return Response.json(
+        { msg: "Invalid or no event type found !" },
+        { status: 403 }
+      );
     }
-    return Response.json({ msg: "Good" }, { status: 200 });
+
+    // If signature verification fails then return `Unauthorized`
+    if (!handleVerifyGithubSignature(req, eventData)) {
+      return Response.json({ msg: "Unauthorized !" }, { status: 401 });
+    }
+
+    // Handle `installation` event type
+    if (
+      eventType === "installation" &&
+      ["created", "deleted", "modified"].includes(eventData?.action)
+    ) {
+      return Response.json({ msg: "Event not handled" }, { status: 200 });
+    }
+
+    // Handle `push` or `pull_request` event type
+    if (
+      eventType === "push" ||
+      (eventType === "pull_request" &&
+        ["opened", "synchronize"].includes(eventData?.action))
+    ) {
+      return Response.json({ msg: "Event handled" }, { status: 200 });
+    }
+
+    // Else send `Event not handled` message
+    return Response.json({ msg: "Event not handled" }, { status: 200 });
   } catch (error) {
     console.log(error);
     return Response.json({ msg: error?.toString() }, { status: 500 });
