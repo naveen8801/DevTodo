0e5a2daa21aca8b72fe00a892613fc03120283ac
naveen8801 <naveen.sharma@stackguardian.io>
2024-03-19 16:26:09 +0530

    Added link to DevTodo heading

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,10 +5,13 @@ import NavBarItems from "./NavBarItems";
 const NavBar: React.FC = (): React.ReactElement => {
   return (
     <div className="w-full h-28 box-border flex gap-8 justify-between items-center px-16">
-      <div className="text-3xl font-extrabold">
-        <span className="text-primaryColor">Dev</span>
-        <span>Todo</span>
-      </div>
+      <Link href={"/"}>
+        <div className="text-3xl font-extrabold">
+          <span className="text-primaryColor">Dev</span>
+          <span>Todo</span>
+        </div>
+      </Link>
+
       <NavBarItems />
     </div>
   );
